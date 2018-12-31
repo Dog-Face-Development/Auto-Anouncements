2ae5e39e7fca58b16805b0d0ceb7272152462553
William <willtheorangeguy@users.noreply.github.com>
2018-12-31 15:23:13 -0800

    Update and add copyright info
    Copyright info updated to 2019

--- send.py
+++ send.py
@@ -1,22 +1,24 @@
-# Copyright (C) 2017 - 2018 William Vandergraaf
+# Copyright (C) 2017 - 2019 Dog Face Development Co.
 
 from email.mime.text import *
 import smtplib
 import time
 import datetime
 
-sendaddress = input("YOUR email address: ") #remove the input statement and replace with your email address for direct delivery
-receiveaddress = input("RECIPIENT's email address: ") #remove the input statement and replace with the reciptents email address for direct delivery
+sendaddress = input('YOUR email address:')
+# replace above with your email address for direct delivery
+receiveaddress = input('RECIPIENT\'s email address:')
+# replace above with the reciptents email address for direct delivery
 
 DATE = str(datetime.datetime.today())
 DATEtoday = str(datetime.date.today())
 print(DATE)
 
 msg = MIMEText(
-   "<h1>Here are your anouncements for today!</h1><p>The announcements for today are atached and available at a server here: %linkhere%</p>","html")
+   '<h1>A Heading</h1><p>Hello There!</p>', 'html')
 
-msg['Subject'] = 'Church Announcements for ', DATEtoday
-msg['From']= sendaddress
+msg['Subject'] = 'Church Announcements for', DATEtoday
+msg['From'] = sendaddress
 msg['To'] = receiveaddress
 
 s = smtplib.SMTP('localhost')
@@ -24,4 +26,4 @@ s.sendmail(sendaddress,
            [receiveaddress],
            msg.as_string())
 
-print("Message sent successfully on ", DATE, "!")
+print('Message sent successfully on', DATE, '!')
